518778d736a4e280137a92a188e02f132f81b3d5
Jean-Emmanuel Beillevert <beillevertj.contact@gmail.com>
2023-10-29 11:00:59 +0100

    starter

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Title app"),
+          centerTitle: true,
+        ),
+        body: MagicBallPage(),
+      ),
+    ),
+  );
+}
+
+class MagicBallPage extends StatefulWidget {
+  const MagicBallPage({super.key});
+
+  @override
+  State<MagicBallPage> createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return;
+  }
+}
